54b66ac293b0e3135de1b9bf95e8acf0517853fe
amira <amira@AMIRA>
2023-02-02 20:41:41 +0000

    Add project files.

---
+++ ProblemN5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemN5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = "hello world";
+            String reverseString = string.Empty;
+            for (int i = S.Length-1; i >= 0; i--)
+            {
+                reverseString += S[i];
+            }
+            Console.WriteLine(reverseString);
+        }
+    }
+}
